84939dc312d604df67a1ab05533014501305b01f
jiangshanmeta <540118044@qq.com>
2021-07-06 00:07:40 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    extends: [
+        'standard',
+        'eslint:recommended',
+        'react-app',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'react-app/jest'
+    ],
+    plugins: [
+        '@typescript-eslint',
+        'react'
+    ],
+    rules: {
+        indent: [
+            'error', 4
+        ]
+    }
+}
