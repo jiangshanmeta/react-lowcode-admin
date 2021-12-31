d8d901fe16e6be73e3d0b67d0a3201b34c5e31c6
jiangshanmeta <540118044@qq.com>
2021-12-31 17:04:49 +0800

    1

--- src/components/common/views/Views.tsx
+++ src/components/common/views/Views.tsx
@@ -5,39 +5,88 @@ import {
     AllOptional
 } from '@/types'
 
-type ViewsProps<T extends Record<string, unknown>, K extends keyof T, U extends {value: T[K]}> = (AllOptional<Omit<U, 'value'>> extends true? {
+type ViewsProps<T extends AnyObject, K extends keyof T, U extends {value: T[K]}> = {
     record: T
     field: K
-    component: {
+} | {
+    record: T
+    field: K
+    component: (AllOptional<Omit<U, 'value'>> extends true? {
         Component: React.FC<U>
         config?: Omit<U, 'value'>
-    }
-} : {
-    record: T
-    field: K
-    component: {
+    }:{
         Component: React.FC<U>
         config: Omit<U, 'value'>
-    }
-}) | {
-    record: T
-    field: K
+    })
 }
 
-export default function Views<T extends AnyObject, K extends keyof T, U extends { value: T[K]}> (props: ViewsProps<T, K, U>): React.ReactNode {
+export default function Views<
+    T extends AnyObject,
+    K extends keyof T,
+    U extends { value: T[K]}
+> (props: ViewsProps<T, K, U>): React.ReactNode {
     const {
         record,
         field
     } = props
     const value = record[field]
     if ('component' in props) {
-        const Component = props.component.Component
-        const props2 = {
+        const {
+            Component,
+            config = {}
+        } = props.component
+
+        const propsData = {
             value,
-            ...(props.component.config ?? null)
+            ...config
         } as U
-        return <Component {...props2} />
+        return <Component {...propsData} />
     }
 
     return <span>{value}</span>
 }
+// 测试类型用的
+// const record = {
+//     name: 'name-123',
+//     id: 'id-21'
+// }
+
+// const ComponentWithoutOtherProp: React.FC<{value: string}> = ({ value }) => {
+//     return <span>{value}</span>
+// }
+
+// const ComponentWithConfig: React.FC<{value: string, suffix: string}> = ({ value, suffix }) => {
+//     return <span>{value}{suffix}</span>
+// }
+
+// const ComponentWithoptionalConfig: React.FC<{value: string, suffix?: string}> = ({ value, suffix = 114 }) => {
+//     return <span>{value}{suffix}</span>
+// }
+
+// Views({
+//     record,
+//     field: 'id',
+//     component: {
+//         Component: ComponentWithoutOtherProp
+//     }
+// })
+// Views({
+//     record,
+//     field: 'id',
+//     component: {
+//         Component: ComponentWithConfig,
+//         config: {
+//             suffix: '--'
+//         }
+//     }
+// })
+// Views({
+//     record,
+//     field: 'id',
+//     component: {
+//         Component: ComponentWithoptionalConfig,
+//         config: {
+//             suffix: '--'
+//         }
+//     }
+// })
